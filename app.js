03a718af51493a50de93233666e22b6c26be1e21
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-24 19:43:17 -0400

    initialize actual server depending on environment

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+const fs = require('fs');
+const env = require('./config');
 const loaders = require('./loaders');
 
 async function startServer() {
 
-    const app = await loaders.init();
+    const app = loaders.init();
 
-    //app.listen
+    let server;
+    if (env.environment === 'production') {
+        server = require('https').createServer(
+            {
+                key: fs.readFileSync(env.https.key),
+                cert: fs.readFileSync(env.https.cert)
+            },
+            app
+        );
+    } else {
+        server = require('http').createServer(app);
+    }
+
+    server.listen(env.port, () => {
+        console.log(`The server is now listening on port ${env.port}.`);
+    });
+
 }
 
 startServer();
