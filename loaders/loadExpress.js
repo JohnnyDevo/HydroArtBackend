dcee7b2bb043996887f71505a1c1b78843c16f64
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-25 17:12:46 -0400

    add cors definitions

--- loaders/loadExpress.js
+++ loaders/loadExpress.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('../config');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 module.exports = {
     expressLoader: function() {
@@ -10,11 +11,18 @@ module.exports = {
         app.get('/status', (req, res) => { res.status(200).end(); });
         app.head('/status', (req, res) => { res.status(200).end(); });
 
+        const corsOptions = {
+            optionsSuccessStatus: 200,
+            credentials: true
+        };
         if (env.environment === 'development') {
             app.use(morgan('dev'));
+            corsOptions.origin = 'http://localhost';
         } else {
             app.use(morgan('tiny'));
+            corsOptions.origin = 'https://johnnydevo.me';
         }
+        app.use(cors(corsOptions));
 
         app.use(bodyParser.urlencoded({ extended: true }));
 
