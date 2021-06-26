e12575223906e1b14794c92cc55c54062f256d87
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-26 14:45:21 -0400

    implement card routes

---
+++ api/cards/index.js
@@ -0,0 +1,33 @@
+const router = require('express-promise-router');
+const cardService = require('../../services/cards');
+
+const cardsRouter = router(); //mounted to '/cards'
+
+cardsRouter.get('/', async (req, res, next) => {
+    try {
+        const cards = await cardService.getAll();
+        res.status(200).send(cards);
+    } catch (error) {
+        console.warn('error occured when getting cards from card service');
+        next(error);
+    }
+});
+
+cardsRouter.get('/search', async (req, res, next) => {
+    try {
+        const search_string = req.query.search;
+        //todo: validate search string here?
+        let cards;
+        if (search_string) {
+            cards = await cardService.search(search_string);
+        } else {
+            cards = await cardService.getAll(search_string);
+        }
+        res.status(200).send(cards);
+    } catch (error) {
+        console.warn('error when searching for cards');
+        next(error);
+    }
+});
+
+module.exports = cardsRouter;
--- loaders/loadRoutes.js
+++ loaders/loadRoutes.js
@@ -1,7 +1,9 @@
+const cardsRouter = require("../api/cards");
 const usersRouter = require("../api/users")
 
 module.exports = {
     routesLoader: function(app) {
         app.use('/users', usersRouter);
+        app.use('/cards', cardsRouter);
     }
 }
