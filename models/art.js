6e5fdbc99368eae534710b53414917f90810a959
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-28 05:34:10 -0400

    set up image submission model

--- models/art.js
+++ models/art.js
@@ -19,5 +19,24 @@ module.exports = {
             console.warn('error occured during card retrieval');
             throw(error);
         }
+    },
+
+    create: async function(cardID, userID, binary) {
+        try {
+            const statement = `
+                INSERT INTO art_submissions
+                VALUES (DEFAULT, $1, $2, $3)
+                RETURNING *;
+            `
+            const arguments = [cardID, userID, binary];
+            const result = await db.query(statement, arguments);
+            if (result.rows?.length) {
+                return result.rows[0];
+            }
+
+        } catch (error) {
+            console.warn('error when inserting image submission into database');
+            throw(error);
+        }
     }
 }
