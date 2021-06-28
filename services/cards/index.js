1ab7488a5f2bed0594e09344681f30ca30400834
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-27 20:06:40 -0400

    add get single card service

--- services/cards/index.js
+++ services/cards/index.js
@@ -15,5 +15,16 @@ module.exports = {
     getAll: async function() {
         const result = await cards.getAll();
         return result;
+    },
+
+    getById: async function(cardID) {
+        //todo: security measures on ID string?
+        if (cardID) {
+            const result = await cards.getById(cardID);
+            return result;
+        } else {
+            console.warn('invalid card ID');
+            throw new Error();
+        }
     }
 }
