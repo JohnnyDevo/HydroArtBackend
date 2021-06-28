dd191408a504219661442a4d5c3526fd79ff9238
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-28 05:02:19 -0400

    define checkArtId middleware

--- api/art/checkArtId.js
+++ api/art/checkArtId.js
@@ -1,3 +1,19 @@
+const artService = require('../../services/art');
+
 module.exports = async function checkArtId(req, res, next) {
-    
+    try {
+        const artID = req.params.artID;
+        //todo: validate id here?
+        if (artID) {
+            const art = await artService.getById(artID);
+            if (art) {
+                req.art = art;
+                return next();
+            }
+        }
+        res.status(404).send();
+    } catch (error) {    
+        console.warn('error when searching for art');
+        next(error);
+    }
 }
