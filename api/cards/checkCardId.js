db70cdd1861bce3c7eb83aa4dc5b660cea4f62cb
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-28 05:03:29 -0400

    structure checkCardId a bit better

--- api/cards/checkCardId.js
+++ api/cards/checkCardId.js
@@ -6,13 +6,14 @@ module.exports = async function checkCard(req, res, next) {
         //todo: validate id here?
         if (cardID) {
             const card = await cardService.getById(cardID);
-            req.card = card;
-            next();
-        } else {
-            res.status(404).send();
+            if (card) {
+                req.card = card;
+                return next();
+            }
         }
+        res.status(404).send();
     } catch (error) {    
-        console.warn('error when searching for cards');
+        console.warn('error when searching for card');
         next(error);
     }
 }
