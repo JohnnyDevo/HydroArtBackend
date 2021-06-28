c20836dbbfb211cb3498930a4893f7f9141a22b4
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-28 07:09:39 -0400

    hook up /art router

--- loaders/loadRoutes.js
+++ loaders/loadRoutes.js
@@ -1,9 +1,11 @@
 const cardsRouter = require("../api/cards");
-const usersRouter = require("../api/users")
+const usersRouter = require("../api/users");
+const artRouter = require("../api/art");
 
 module.exports = {
     routesLoader: function(app) {
         app.use('/users', usersRouter);
         app.use('/cards', cardsRouter);
+        app.use('/art', artRouter);
     }
 }
