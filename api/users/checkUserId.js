21e9a6de47cfe27c96730f36e212fd3d2bbd1b80
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-28 05:06:03 -0400

    define checkUserId middleware

--- api/users/checkUserId.js
+++ api/users/checkUserId.js
@@ -1,3 +1,19 @@
+const userService = require('../../services/users');
+
 module.exports = async function checkUserId(req, res, next) {
-    
+    try {
+        const userID = req.params.userID;
+        //todo: validate id here?
+        if (userID) {
+            const foundUser = await userService.findUser(userID);
+            if (foundUser) {
+                req.foundUser = foundUser;
+                return next();
+            }
+        }
+        res.status(404).send();
+    } catch (error) {    
+        console.warn('error when searching for user');
+        next(error);
+    }
 }
