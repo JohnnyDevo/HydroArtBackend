690fec0b362a046aa64af6cd6550ccce33117246
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-28 08:32:42 -0400

    create checkpoints to make sure the image is valid

--- services/acceptImage.js
+++ services/acceptImage.js
@@ -1,14 +1,40 @@
 const multer = require('multer');
+const mmm = require('mmmagic');
+const Magic = mmm.Magic;
+const magic = new Magic(mmm.MAGIC_MIME_TYPE);
 
 const storage = multer.memoryStorage(); //since I'm just using my database, I'll send the buffer directly to postgres
 
 function fileFilter(req, file, cb) {
-    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/bmp") { //only accept basic image types
+    if (testMimetype(file.mimetype)) { //before file is received, test declared mimetype
         cb(null, true);
       } else {
         cb(null, false);
         return cb(new Error('Only .png, .jpg and .bmp format allowed!'));
       }
+}
+
+function interpretFile(req, res, next) { //analyze the file to find actual type
+    magic.detect(req.file.buffer, (err, result) => { 
+        if (err) {
+            console.warn('error occurred when interpreting file');
+            next(err);
+        }
+        if (testMimetype(result)) { //test actual mimetype
+            next();
+        } else {
+            console.warn('invalid image');
+            next(new Error());
+        }
+    });
+}
+
+function testMimetype(mimetype) {
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpeg" || file.mimetype == "image/bmp") { //only accept basic image types
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const limits = {
@@ -18,8 +44,11 @@ const limits = {
     parts: 2
 }
 
-module.exports = multer({
-    storage, storage,
-    fileFilter: fileFilter,
-    limits: limits
-});
+module.exports = {
+    acceptFile: multer({
+        storage, storage,
+        fileFilter: fileFilter,
+        limits: limits
+    }),
+    interpretFile: interpretFile
+};
