92ff8d3e2af288efec4c28f397e7e8bbb1e4fdd1
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-28 08:08:11 -0400

    fileFilter does not have access to file buffer

--- services/acceptImage.js
+++ services/acceptImage.js
@@ -1,27 +1,25 @@
 const multer = require('multer');
-const Magic = require('mmmagic').Magic;
-const magic = new Magic();
 
 const storage = multer.memoryStorage(); //since I'm just using my database, I'll send the buffer directly to postgres
 
 function fileFilter(req, file, cb) {
-    if (file.size > 1000000) {
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/bmp") { //only accept basic image types
+        cb(null, true);
+      } else {
         cb(null, false);
-        return cb(new Error('1MB file size limit (please have mercy on my tiny server)'));
-    }
-    
-    magic.detect(file.buffer, (err, result) => { //why doesn't this have await functionality :(
-        if (err) {
-            console.warn('error occurred when interpreting file');
-            cb(null, false);
-            return cb(err);
-        }
-        console.log(`mmmagic's interpretation of the uploaded file is:`);
-        console.log(result);
-    });
-} 
+        return cb(new Error('Only .png, .jpg and .bmp format allowed!'));
+      }
+}
+
+const limits = {
+    files: 1,
+    fileSize: 1000000,
+    fields: 1,
+    parts: 2
+}
 
 module.exports = multer({
-    storage: storage,
-    fileFilter: fileFilter
+    storage, storage,
+    fileFilter: fileFilter,
+    limits: limits
 });
